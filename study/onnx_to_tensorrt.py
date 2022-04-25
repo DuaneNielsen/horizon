7296e7bf0a4c22b68d43015374401b8407af9cea
duanenielsen <duane.nielsen.rocks@gmail.com>
2022-04-24 21:10:24 -0700

    Added tests for TensorRT

---
+++ study/onnx_to_tensorrt.py
@@ -0,0 +1,43 @@
+import tensorrt as trt
+
+
+from torchvision import models
+model = models.resnet50(pretrained=True)
+
+
+# logger to capture errors, warnings, and other information during the build and inference phases
+TRT_LOGGER = trt.Logger()
+
+
+def build_engine(onnx_file_path):
+    # initialize TensorRT engine and parse ONNX model
+    builder = trt.Builder(TRT_LOGGER)
+    network = builder.create_network()
+    parser = trt.OnnxParser(network, TRT_LOGGER)
+
+    # parse ONNX
+    with open(onnx_file_path, 'rb') as model:
+        print('Beginning ONNX file parsing')
+        parser.parse(model.read())
+    print('Completed parsing of ONNX file')
+
+    # allow TensorRT to use up to 1GB of GPU memory for tactic selection
+    builder.max_workspace_size = 1 << 30
+    # we have only one image in batch
+    builder.max_batch_size = 1
+    # use FP16 mode if possible
+    if builder.platform_has_fast_fp16:
+        builder.fp16_mode = True
+
+    # generate TensorRT engine optimized for the target platform
+        print('Building an engine...')
+        engine = builder.build_cuda_engine(network)
+        context = engine.create_execution_context()
+        print("Completed creating Engine")
+
+        return engine, context
+
+
+def main():
+    # initialize TensorRT engine and parse ONNX model
+    engine, context = build_engine('ImageClassifier.onnx')
